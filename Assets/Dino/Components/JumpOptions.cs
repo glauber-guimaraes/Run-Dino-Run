9413dae8bf46c65ca860b4f119c1a7783a9b1dac
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2019-06-28 18:34:26 -0300

    Add explanations for gravity and jump velocity

--- Assets/Dino/Components/JumpOptions.cs
+++ Assets/Dino/Components/JumpOptions.cs
@@ -1,5 +1,26 @@
 ﻿using Unity.Entities;
 
+/*
+ * A little explanation about the values of gravity and jumpVelocity found in the inspector :
+ * 
+ * Instead of trying value by value until the jump feels right, we can just use the physics formulas
+ * with the desired jump behaviour to compute those values.
+ * 
+ * For the equations of movement we have that:
+ * v = v_0 + a*t 
+ * v^2 = (v_0)^2 + 2*a*deltaS
+ * 
+ * As we're only interested in the vertical component (a = gravity), we can define 2 constants for the jump :
+ * | t_max - Time until maximum height
+ * | s_max - Maximum height during the jump
+ * 
+ * If we substitute both into the original equations, when the height is maximum, i.e. v = 0:
+ * 0 = jumpVelocity + gravity * t_max
+ * 0 = (jumpVelocity)^2 + 2 * gravity * s_max
+ * 
+ * Solving those 2 equations gives the gravity and jumpVelocity.
+ */
+
 public struct JumpOptions : IComponentData
 {
     public float fallMultiplier;
